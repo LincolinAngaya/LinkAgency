be6e02627459960abd7f3627cd8d171cb490b684
LincolinAngaya <lincolinangaya5626@gmail.com>
2024-02-05 22:44:30 +0300

    import sections

--- linkagency/src/App.jsx
+++ linkagency/src/App.jsx
@@ -1,7 +1,21 @@
+import About from "./Section/About";
+import Contact from "./Section/Contact";
+import Footer from "./Section/Footer";
+import Header from "./Section/Header";
+import Hero from "./Section/Hero";
+import Pricing from "./Section/Pricing";
+import Testimonials from "./Section/Testimonials";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <Header />
+    <Hero />
+    <About />
+    <Testimonials />
+    <Pricing />
+    <Contact />
+    <Footer />
+    </>
   )
 }
