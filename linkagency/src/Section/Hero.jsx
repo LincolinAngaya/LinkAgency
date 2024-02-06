c4eaa7c47978d126011549171b9e7378ecb67d75
LincolinAngaya <lincolinangaya5626@gmail.com>
2024-02-06 17:57:41 +0300

    done styling the section of the hero

--- linkagency/src/Section/Hero.jsx
+++ linkagency/src/Section/Hero.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import heroImg from '../assets/images/heroImg.png'
 function Hero() {
   return (
-    <div>Hero</div>
+   <section className='flex flex-col lg:flrx-row justify-between items-center gap-4 w-full bg-slate-100 h-auto lg:h-screen'>
+   </section>
   )
 }
 
