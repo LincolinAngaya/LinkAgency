b57ee9258e60ae8f0b494e954727280c5fd86268
LincolinAngaya <lincolinangaya5626@gmail.com>
2024-02-06 18:02:31 +0300

    style dic container with h1 tag

--- linkagency/src/Section/Hero.jsx
+++ linkagency/src/Section/Hero.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-import heroImg from '../assets/images/heroImg.png'
+// import heroImg from '../assets/images/heroImg.png'
 function Hero() {
   return (
    <section className='flex flex-col lg:flrx-row justify-between items-center gap-4 w-full bg-slate-100 h-auto lg:h-screen'>
+   <div className='flex justify-center items-start flex-col gap-8 lg:w-1/2 px-10 py-10 lg:px-20 lg:py-20'>
+   <h1 className='text-green-600 font-bold text-6x1'>One click solution for static websuite</h1>
+   </div>
    </section>
   )
 }
