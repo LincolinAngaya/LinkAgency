56bf77b3d0c1989428fafbfdec81d43f93b3bbca
LincolinAngaya <lincolinangaya5626@gmail.com>
2024-02-07 18:44:06 +0300

    ADD description

--- linkagency/src/Section/About.jsx
+++ linkagency/src/Section/About.jsx
@@ -4,7 +4,8 @@ function About() {
   return (
  <section id='services' className='w-full bg-slate-200 flex flex-col lg:flex-row gap-10 h-fit px-7 py-4 lg:px-12'>
  <div className='flex justify-center items-start flex-col gap-8 lg:w-1/2'>
- <h1 className='text-green-800 font-bold text-6x1'> Elevating Businesses Globally with Creative Digital Solutions.z</h1>
+ <h1 className='text-green-800 font-bold text-6x1'> Elevating Businesses with Creative Digital Solutions.</h1>
+ <p className='text-slate-05 text-2x1'>Digital agency crafting tailored solutions. Elevate credibility, expand reach, and boost sales with our creative technology expertise.</p>
  </div>
  </section>
   )
