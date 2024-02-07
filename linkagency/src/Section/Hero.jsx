edb0d79978353b888879335d08bbe9440273f813
LincolinAngaya <lincolinangaya5626@gmail.com>
2024-02-07 18:09:52 +0300

    add hero image

--- linkagency/src/Section/Hero.jsx
+++ linkagency/src/Section/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-// import heroImg from '../assets/images/heroImg.png'
+import heroImg from '../assets/images/heroImg.png'
 function Hero() {
   return (
    <section className='flex flex-col lg:flex-row justify-between items-center gap-4 w-full bg-slate-100 h-auto lg:h-screen'>
@@ -11,7 +11,8 @@ function Hero() {
    <button className='border-2 border-green-800 text-green-800  px-4 lg:px-8 py-3 rounded-md text-[18px] hover:border-black hover:text-black cursor-pointer'>Watch Video</button>
    </div>
    </div>
-   <div className='flex justify-center items-center '>
+   <div className='flex justify-center items-center w-1/2 px-5 py-20 object-cover'>
+    <img src={heroImg} alt='hero image' width={500} height={500} />
    </div>
    </section>
   )
