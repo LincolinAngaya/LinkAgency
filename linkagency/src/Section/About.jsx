aa4f46ac27f58fc9f7ec792968b6c90c1d9d2bd3
LincolinAngaya <lincolinangaya5626@gmail.com>
2024-02-07 18:38:35 +0300

    add about us

--- linkagency/src/Section/About.jsx
+++ linkagency/src/Section/About.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 function About() {
   return (
-    <div>
-      about
-    </div>
+ <section id='services' className='w-full bg-slate-200 flex flex-col lg:flex-row gap-10 h-fit px-7 py-4 lg:px-12'>
+ <div className='flex justify-center items-start flex-col gap-8 lg:w-1/2'>
+ <h1 className='text-green-800 font-bold text-6x1'> Elevating Businesses Globally with Creative Digital Solutions.z</h1>
+ </div>
+ </section>
   )
 }
 
--- linkagency/src/Section/Hero.jsx
+++ linkagency/src/Section/Hero.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import heroImg from '../assets/images/heroImg.png'
+import heroImg from '../assets/images/heroimg.png'
 function Hero() {
   return (
    <section className='flex flex-col lg:flex-row justify-between items-center gap-4 w-full bg-slate-100 h-auto lg:h-screen'>
    <div className='flex justify-center items-start flex-col gap-8 lg:w-1/2 px-10 py-10 lg:px-20 lg:py-20'>
-   <h1 className='text-green-600 font-bold text-6x1'>One click solution for static websuite</h1>
-   <p className='text-slate-90 text-2xl'>The error message you're seeing indicates a version mismatch between your installed Ruby version and the Ruby version specified in your Gemfile. To resolve this issue, you can do the following:</p>
+   <h1 className='text-green-600 font-bold text-6x1'>Unlock the Potential of Your Digital World With Our</h1>
+   <p className='text-slate-90 text-2xl'>Unlock your online potential with our exceptional team of content experts, design maestros, and development gurus. Committed to elevating your business and expanding your online reach, we are here to make your dreams a reality.</p>
    <div className='flex justify-center items-center gap-7'>
    <button className='bg-green-800 text-white px-4 lg:px-8 py-3 rounded-md text-[18px] hover:bg-black hover:text-white cursor-pointer'>View More</button>
    <button className='border-2 border-green-800 text-green-800  px-4 lg:px-8 py-3 rounded-md text-[18px] hover:border-black hover:text-black cursor-pointer'>Watch Video</button>
