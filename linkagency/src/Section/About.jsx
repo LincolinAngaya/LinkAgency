285e3163a791cbe5dd9280c63b9bd570c0401df2
LincolinAngaya <lincolinangaya5626@gmail.com>
2024-02-07 18:46:43 +0300

    add button

--- linkagency/src/Section/About.jsx
+++ linkagency/src/Section/About.jsx
@@ -6,6 +6,9 @@ function About() {
  <div className='flex justify-center items-start flex-col gap-8 lg:w-1/2'>
  <h1 className='text-green-800 font-bold text-6x1'> Elevating Businesses with Creative Digital Solutions.</h1>
  <p className='text-slate-05 text-2x1'>Digital agency crafting tailored solutions. Elevate credibility, expand reach, and boost sales with our creative technology expertise.</p>
+ <div className='flex justify-center item-center gap-7'>
+ <button>Read More</button>
+ </div>
  </div>
  </section>
   )
